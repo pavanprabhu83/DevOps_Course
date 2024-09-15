e9f03113bc1075b5e9aada7d9e98b533b44e2f33
pavanprabhu83 <pavanprabhu@hotmail.com>
2024-09-15 12:54:04 +0530

    update to bucket

--- s3_Demo/main.tf
+++ s3_Demo/main.tf
@@ -1,8 +1,19 @@
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "my_bucket" {
   bucket = "devops-course-pavan-s3-bucket-140924"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_my_bucket" {
+  bucket = aws_s3_bucket.my_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+output "s3_bucket_id" {
+  value = aws_s3_bucket.my_bucket.id
 }
