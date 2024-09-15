cd6eec61defb00f52072aad701ff96b15463c95d
pavanprabhu83 <pavanprabhu@hotmail.com>
2024-09-15 13:00:03 +0530

    update

--- s3_Demo/main.tf
+++ s3_Demo/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "example" {
   bucket = "devops-course-pavan-s3-bucket-140924"
 
   tags = {
@@ -7,13 +7,13 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 }
 
-resource "aws_s3_bucket_versioning" "versioning_my_bucket" {
-  bucket = aws_s3_bucket.my_bucket.id
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 output "s3_bucket_id" {
-  value = aws_s3_bucket.my_bucket.id
+  value = aws_s3_bucket.example.id
 }
