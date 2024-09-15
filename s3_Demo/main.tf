d17f1e42dedc9e4d52d7e1326766b2d8b3ce8009
pavanprabhu83 <pavanprabhu@hotmail.com>
2024-09-15 13:27:07 +0530

    added assignment

--- s3_Demo/main.tf
+++ s3_Demo/main.tf
@@ -2,8 +2,8 @@ resource "aws_s3_bucket" "example" {
   bucket = "devops-course-pavan-s3-bucket-140924"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+  Name        = "My bucket"
+    Environment = "Dev"  
   }
 }
 
