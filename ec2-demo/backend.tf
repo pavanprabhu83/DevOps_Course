d6b6f1001b03770c8583a301d614aed9070b57d5
pavanprabhu83 <pavanprabhu@hotmail.com>
2024-09-17 02:49:54 +0530

    adding new folder for ec2 demo

---
+++ ec2-demo/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "devops-sept2024-terraform-backend-ec2"
+    key    = "terraform-backend/"
+    region = "eu-west-1"
+  }
+}
---
+++ ec2-demo/instance.tf
@@ -0,0 +1,79 @@
+resource "aws_instance" "webserver01" {
+    ami = "ami-0fa8fe6f147dc938b"
+    # ami = var.image_id
+
+    instance_type = "t2.micro"
+
+    # tags = var.default_tags
+
+    tags = {
+        Name = "Amazon-Linux Test Machine"
+        email = "pavan@gmail.com"
+    }
+    //key_name = "ap-south-1-terraform-demo"
+    vpc_security_group_ids = [aws_security_group.mysg.id]
+}
+
+resource "aws_iam_role" "ssm_role" {
+  name = "ssm_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+      Name = "EC2 ssm profile"
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = "${aws_iam_role.ssm_role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm-policy" {
+role       = aws_iam_role.ssm_role.name
+policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_security_group"  "mysg" {
+    egress = [
+        {
+            cidr_blocks = ["0.0.0.0/0",]
+            description = "Pavans home network"
+            from_port = 0
+            protocol = "-1"
+            to_port = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            security_groups = []
+            self = false
+        }
+    ]
+
+    ingress = [
+        {
+            cidr_blocks = ["0.0.0.0/0",]
+            description = "Pavans home network"
+            from_port = 0
+            protocol = "-1"
+            to_port = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            security_groups = []
+            self = false
+            }
+        ]
+}
