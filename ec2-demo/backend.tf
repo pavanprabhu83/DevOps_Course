0fccf2b1df160ecd4141aaca95e3a8a368db3e19
pavanprabhu83 <pavanprabhu@hotmail.com>
2024-09-17 02:51:01 +0530

    update

--- ec2-demo/backend.tf
+++ ec2-demo/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "devops-sept2024-terraform-backend-ec2"
-    key    = "terraform-backend/"
+    key    = "terraform-backend"
     region = "eu-west-1"
   }
 }
